ea1fe583ee1f7d969eea1430832f8857835262c1
Huelsenfrucht <michael.rothammer@protonmail.com>
2024-09-04 15:06:20 +0200

    added improved examples

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -29,12 +29,35 @@ export default function App() {
     <View style={styles.container}>
       {Platform.OS === 'android' && (
         <>
-          <Text>Haptic support level: {androidHapticSupportLevel}</Text>
+          <Text>
+            Haptic support level:{' '}
+            {
+              /**
+               * This value indicates the device's haptic support level.
+               * The possible values are:
+               * - `0`: The device does not support haptics.
+               * - `1`: The device supports on/off haptic feedback.
+               * - `2`: The device supports amplitude control haptic feedback.
+               * - `3`: The device supports fully customizable haptic feedback.
+               */
+              androidHapticSupportLevel
+            }
+          </Text>
           <Button
             title="Play Android Haptics"
             onPress={() => {
+              /**
+               * This command will play a haptic pattern from the directory
+               * `Android samples/Double click with audio-Simple pattern-Single Vibration`.
+               * Depending on the device's haptic support level, different haptic
+               * feedback will be played.
+               * For haptic support level 3, "Double click with audio" will be played.
+               * For haptic support level 2, "Simple pattern" will be played.
+               * For haptic support level 1, "Single Vibration" will be played.
+               * For haptic support level 0, no haptic feedback will be played.
+               */
               playAndroidHaptics(
-                'Ramp + click2-Vibration A-Subtle Repetitive Notification'
+                'Android samples/Double click with audio-Simple pattern-Single Vibration'
               ).then(() => {
                 console.log('Android haptics played');
               });
@@ -43,8 +66,14 @@ export default function App() {
           <Button
             title="Play HLA"
             onPress={() => {
+              /**
+               * This command will play the .hla file located at
+               * `Android samples/Double click with audio/lvl2/main.hla`.
+               * Note that this .hla file references an audio file, which will be played
+               * along with the haptic feedback.
+               */
               playHLA(
-                'Ramp + click2-Vibration A-Subtle Repetitive Notification/lvl2/main.hla'
+                'Android samples/Double click with audio/lvl2/main.hla'
               ).then(() => {
                 console.log('HLA played');
               });
@@ -53,11 +82,17 @@ export default function App() {
           <Button
             title="Play OGG"
             onPress={() => {
-              playOGG(
-                'Ramp + click2-Vibration A-Subtle Repetitive Notification/lvl3/main.ogg'
-              ).then(() => {
-                console.log('OGG played');
-              });
+              /**
+               * This command will play the .ogg file located at
+               * `Ramp + click2-Vibration A-Subtle Repetitive Notification/lvl3/main.ogg`.
+               * Note that .ogg playback requires the device to support haptics level 3:
+               * If the device's haptic support is less than 3, no haptic feedback will be played.
+               */
+              playOGG('Android samples/Simple pattern/lvl3/main.ogg').then(
+                () => {
+                  console.log('OGG played');
+                }
+              );
             }}
           />
         </>
